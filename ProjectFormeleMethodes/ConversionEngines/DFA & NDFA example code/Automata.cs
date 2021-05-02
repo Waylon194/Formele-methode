13d6efa82623e2add7f2776da9d1632264e07349
Waylon <waylon194@hotmail.com>
2021-05-02 14:50:11 +0200

    Added .vs to .gitignore & converted java example to C#
    Code is nog niet werkend/compleet, maar een aardig gedeelte is al klaar.

---
+++ ProjectFormeleMethodes/ConversionEngines/DFA & NDFA example code/Automata.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// The class Automata represents both DFA and NDFA: some NDFA's are also DFA
+/// Using the method isDFA we can check this
+/// 
+/// We use '$' to denote the empty symbol epsilon
+/// 
+/// @author Paul de Mast
+/// @version 1.0
+/// 
+/// </summary>
+
+public class Automata<T> where T : IComparable
+{
+	// Or use a Map structure
+	private ISet<Transition <T>> transitions;
+	private SortedSet<T> states;
+	private SortedSet<T> startStates;
+	private SortedSet<T> finalStates;
+	private SortedSet<char> symbols;
+
+	public Automata() : this(new SortedSet<char>())
+	{
+
+	}
+
+	public Automata(char?[] s) : this(new SortedSet<char>(Arrays.asList(s)))
+	{
+
+	}
+
+	public Automata(SortedSet<char> symbols)
+	{
+		transitions = new SortedSet<Transition<T>>();
+		states = new SortedSet<T>();
+		startStates = new SortedSet<T>();
+		finalStates = new SortedSet<T>();
+		this.setAlphabet(symbols);
+	}
+
+	public virtual char?[] Alphabet
+	{
+		set
+		{
+			this.setAlphabet(new SortedSet<char>(Arrays.asList(value)));
+		}
+		get
+		{
+		   return symbols;
+		}
+	}
+
+	public virtual SortedSet<char> Alphabet
+	{
+		set
+		{
+		   this.symbols = value;
+		}
+	}
+
+	public virtual void addTransition(Transition<T> t)
+	{
+		transitions.Add(t);
+		states.Add(t.FromState);
+		states.Add(t.ToState);
+	}
+
+	public virtual void defineAsStartState(T t)
+	{
+		// if already in states no problem because a Set will remove duplicates.
+		states.Add(t);
+		startStates.Add(t);
+	}
+
+	public virtual void defineAsFinalState(T t)
+	{
+		// if already in states no problem because a Set will remove duplicates.
+		states.Add(t);
+		finalStates.Add(t);
+	}
+
+	public virtual void printTransitions()
+	{
+		foreach (Transition<T> t in transitions)
+		{
+			Console.WriteLine(t);
+		}
+	}
+
+	public virtual bool DFA
+	{
+		get
+		{
+			bool isDFA = true;
+    
+			foreach (T from in states)
+			{
+				foreach (char symbol in symbols)
+				{
+					isDFA = isDFA && getToStates(from, symbol).size() == 1;
+				}
+			}
+    
+			return isDFA;
+		}
+	}
+}
---
+++ ProjectFormeleMethodes/ConversionEngines/DFA & NDFA example code/Transition.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+/// <summary>
+/// The class Automata represents both DFA and NDFA: some NDFA's are also DFA
+/// Using the method isDFA we can check this
+/// 
+/// We use '$' to denote the empty symbol epsilon
+/// 
+/// @author Paul de Mast
+/// @version 1.0
+/// </summary>
+
+public class Transition<T> : IComparable<Transition<T>> where T : IComparable
+{
+	public const char EPSILON = '$';
+	private T fromState;
+	private char symbol;
+	private T toState;
+
+   // this constructor can be used to define loops:
+	public Transition(T fromOrTo, char s) : this(fromOrTo, s, fromOrTo)
+	{
+
+	}
+
+	public Transition(T from, T to) : this(from, EPSILON, to)
+	{
+
+	}
+
+	public Transition(T from, char s, T to)
+	{
+		this.fromState = from;
+		this.symbol = s;
+		this.toState = to;
+	}
+
+	// overriding equals
+	public override bool Equals(object other)
+	{
+	   if (other == null)
+	   {
+		  return false;
+	   }
+	   else if (other is Transition)
+	   {
+			   return this.fromState.Equals(((Transition)other).fromState) && this.toState.Equals(((Transition)other).toState) && this.symbol == (((Transition)other).symbol);
+	   }
+	   else
+	   {
+		   return false;
+	   }
+	}
+
+//JAVA TO C# CONVERTER TODO TASK: Most Java annotations will not have direct .NET equivalent attributes:
+//ORIGINAL LINE: @SuppressWarnings("unchecked") public int compareTo(Transition<T> t)
+	public int CompareTo(Transition<T> t)
+	{
+		int fromCmp = fromState.CompareTo(t.fromState);
+		int symbolCmp = (new char?(symbol)).compareTo(new char?(t.symbol));
+		int toCmp = toState.CompareTo(t.toState);
+
+		return (fromCmp != 0 ? fromCmp : (symbolCmp != 0 ? symbolCmp : toCmp));
+	}
+
+	public T FromState
+	{
+		get
+		{
+			return fromState;
+		}
+	}
+
+	public T ToState
+	{
+		get
+		{
+			return toState;
+		}
+	}
+
+	public char Symbol
+	{
+		get
+		{
+			return symbol;
+		}
+	}
+
+	public override string ToString()
+	{
+	   return "(" + this.FromState + ", " + this.Symbol + ")" + "-->" + this.ToState;
+	}
+}
