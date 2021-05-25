75af5dd6fb1e2d0a4981236e044b644004241c1f
Vincent <vincent.wjap.de.rooij@outlook.com>
2021-05-25 16:49:02 +0200

    Fixes to ensure compilation and running the app.

--- ProjectFormeleMethodes/ConversionEngines/DFA & NDFA example code/Automata.cs
+++ ProjectFormeleMethodes/ConversionEngines/DFA & NDFA example code/Automata.cs
@@ -15,71 +15,73 @@ using System.Linq;
 /// </summary>
 
 namespace ProjectFormeleMethodes.NDFA
-public class Automata<T> where T : IComparable
 {
-	public ISet<Transition<T>> Transitions { get; }
-	public SortedSet<T> States { get; }
-	public SortedSet<T> StartStates { get; set; }
-	public SortedSet<T> FinalStates { get; set; }
-	public SortedSet<char> Symbols { get; set; }
-
-	public Automata() : this(new SortedSet<char>())
-	{
-	}
-
-	public Automata(char[] s) : this(new SortedSet<char>(s))
-	{
-	}
-
-	public Automata(SortedSet<char> symbols)
-	{
-		Transitions = new SortedSet<Transition<T>>();
-		States = new SortedSet<T>();
-		StartStates = new SortedSet<T>();
-		FinalStates = new SortedSet<T>();
-		Symbols = symbols;
-	}
-
-	public void AddTransition(Transition<T> t)
-	{
-		Transitions.Add(t);
-		States.Add(t.FromState);
-		States.Add(t.ToState);
-	}
-
-	public void DefineAsStartState(T t)
-	{
-		if (!States.Contains(t))
-		{
-			States.Add(t);
-		}
-		StartStates.Add(t);
-	}
-
-	public void DefineAsFinalState(T t)
-	{
-		if (!States.Contains(t))
-		{
-			States.Add(t);
-		}
-		FinalStates.Add(t);
-	}
-
-	public List<Transition<T>> GetToStates(T state, char symbol)
-	{
-		return Transitions.Where(e => e.Symbol == symbol).Where(e => e.FromState.Equals(state)).ToList();
-	}
-
-	public bool IsDfa()
+    public class Automata<T> where T : IComparable 
     {
-        bool isDfa = !(Transitions.Where(e => e.Symbol.Equals('$')).ToList().Count > 0);
-        foreach (T state in States)
+        public ISet<Transition<T>> Transitions { get; }
+        public SortedSet<T> States { get; }
+        public SortedSet<T> StartStates { get; set; }
+        public SortedSet<T> FinalStates { get; set; }
+        public SortedSet<char> Symbols { get; set; }
+
+        public Automata() : this(new SortedSet<char>())
         {
-            foreach (char symbol in Symbols)
-            {
-                isDfa = isDfa && GetToStates(state, symbol).Count <= 1;
-            }
         }
-        return isDfa;
+
+        public Automata(char[] s) : this(new SortedSet<char>(s))
+        {
+        }
+
+        public Automata(SortedSet<char> symbols)
+        {
+            Transitions = new SortedSet<Transition<T>>();
+            States = new SortedSet<T>();
+            StartStates = new SortedSet<T>();
+            FinalStates = new SortedSet<T>();
+            Symbols = symbols;
+        }
+
+        public void AddTransition(Transition<T> t)
+        {
+            Transitions.Add(t);
+            States.Add(t.FromState);
+            States.Add(t.ToState);
+        }
+
+        public void DefineAsStartState(T t)
+        {
+            if (!States.Contains(t))
+            {
+                States.Add(t);
+            }
+            StartStates.Add(t);
+        }
+
+        public void DefineAsFinalState(T t)
+        {
+            if (!States.Contains(t))
+            {
+                States.Add(t);
+            }
+            FinalStates.Add(t);
+        }
+
+        public List<Transition<T>> GetToStates(T state, char symbol)
+        {
+            return Transitions.Where(e => e.Symbol == symbol).Where(e => e.FromState.Equals(state)).ToList();
+        }
+
+        public bool IsDfa()
+        {
+            bool isDfa = !(Transitions.Where(e => e.Symbol.Equals('$')).ToList().Count > 0);
+            foreach (T state in States)
+            {
+                foreach (char symbol in Symbols)
+                {
+                    isDfa = isDfa && GetToStates(state, symbol).Count <= 1;
+                }
+            }
+            return isDfa;
+        }
     }
 }
