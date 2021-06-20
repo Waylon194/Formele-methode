3a224381faf27a58ea7f156ef55750e25bb55151
Waylon <waylon194@hotmail.com>
2021-06-20 16:20:25 +0200

    Pushed final methodes to Automata.cs

--- ProjectFormeleMethodes/NFA-DFA/Automata.cs
+++ ProjectFormeleMethodes/NFA-DFA/Automata.cs
@@ -3,19 +3,6 @@ using System.Collections.Generic;
 using ProjectFormeleMethodes.NDFA.Transitions;
 using System.Linq;
 
-/// <summary>
-/// The class Automata represents both DFA and NDFA: some NDFA's are also DFA
-/// Using the method isDFA we can check this
-/// 
-/// We use '$' to denote the empty symbol epsilon
-/// 
-/// @author Paul de Mast
-/// @version 1.0
-
-// Updates* With edits from Waylon Lodder & Vincent de Rooij to fix formatting from Java to C#
-
-/// </summary>
-
 namespace ProjectFormeleMethodes.NDFA
 {
     public class Automata<T> where T : IComparable
@@ -118,6 +105,204 @@ namespace ProjectFormeleMethodes.NDFA
                 transitions.AddRange(GetTransition(epsilonState));
             }
             return transitions;
+        }
+
+        private static void BeginsWith(DfaGenerateValue param, ref Automata<string> dfa)
+        {
+            char[] chars = param.Parameter.ToCharArray();
+            int stateCounter = 0;
+            dfa.DefineAsStartState(stateCounter.ToString());
+            foreach (char c in chars)
+            {
+                dfa.AddTransition(new Transition<string>(stateCounter.ToString(), c,
+                    (stateCounter + 1).ToString()));
+
+                stateCounter = dfa.States.Count - 1;
+            }
+
+            dfa.DefineAsFinalState(stateCounter.ToString());
+            foreach (char c in dfa.Symbols)
+            {
+                dfa.AddTransition(new Transition<string>(stateCounter.ToString(), c, stateCounter.ToString()));
+            }
+
+            //Hardcopy states
+            SortedSet<string> ogStates = new SortedSet<string>();
+            foreach (string state in dfa.States)
+            {
+                ogStates.Add(state);
+            }
+
+            foreach (string state in ogStates)
+            {
+                List<Transition<string>> trans = dfa.GetTransition(state);
+                SortedSet<char> routesPresent = new SortedSet<char>();
+                foreach (Transition<string> t in trans)
+                {
+                    routesPresent.Add(t.Symbol);
+                }
+
+                foreach (char letter in dfa.Symbols)
+                {
+                    if (!routesPresent.Contains(letter))
+                    {
+                        dfa.AddTransition(new Transition<string>(state, letter, "F"));
+                    }
+                }
+            }
+
+            if (dfa.States.Contains("F"))
+            {
+                foreach (char letter in dfa.Symbols)
+                {
+                    dfa.AddTransition(new Transition<string>("F", letter, "F"));
+                }
+            }
+        }
+
+        private static void Contains(DfaGenerateValue param, ref Automata<string> dfa)
+        {
+            char[] chars = param.Parameter.ToCharArray();
+            int stateCounter = 0;
+            dfa.DefineAsStartState(stateCounter.ToString());
+            foreach (char c in chars)
+            {
+                dfa.AddTransition(new Transition<string>(stateCounter.ToString(), c,
+                    (stateCounter + 1).ToString()));
+                stateCounter = dfa.States.Count - 1;
+            }
+
+            dfa.DefineAsFinalState(stateCounter.ToString());
+
+            //Hardcopy states
+            List<string> ogStates = new List<string>();
+            foreach (string state in dfa.States)
+            {
+                ogStates.Add(state);
+            }
+
+            for (int i = 0; i < ogStates.Count; i++)
+            {
+                string state = ogStates[i];
+                List<Transition<string>> trans = dfa.GetTransition(state);
+                SortedSet<char> routesPresent = new SortedSet<char>();
+                foreach (Transition<string> t in trans)
+                {
+                    routesPresent.Add(t.Symbol);
+                }
+
+                foreach (char letter in dfa.Symbols)
+                {
+                    if (!routesPresent.Contains(letter) && !dfa.FinalStates.Contains(state))
+                    {
+                        int stateToReturnTo = backTrackForWorkingRoute(chars, letter);
+                        dfa.AddTransition(new Transition<string>(state, letter, ogStates[stateToReturnTo]));
+                    }
+                }
+            }
+
+            foreach (char c in dfa.Symbols)
+            {
+                foreach (string finalstate in dfa.FinalStates)
+                    dfa.AddTransition(new Transition<string>(stateCounter.ToString(), c, stateCounter.ToString()));
+            }
+        }
+
+        private static void EndsWith(DfaGenerateValue param, ref Automata<string> dfa)
+        {
+            char[] chars = param.Parameter.ToCharArray();
+            int stateCounter = 0;
+            dfa.DefineAsStartState(stateCounter.ToString());
+            foreach (char c in chars)
+            {
+                dfa.AddTransition(new Transition<string>(stateCounter.ToString(), c,
+                    (stateCounter + 1).ToString()));
+
+                stateCounter = dfa.States.Count - 1;
+            }
+
+            dfa.DefineAsFinalState(stateCounter.ToString());
+            //Hardcopy states
+            List<string> ogStates = new List<string>();
+            foreach (string state in dfa.States)
+            {
+                ogStates.Add(state);
+            }
+
+            for (int i = 0; i < ogStates.Count; i++)
+            {
+                string state = ogStates[i];
+                List<Transition<string>> trans = dfa.GetTransition(state);
+                SortedSet<char> routesPresent = new SortedSet<char>();
+                foreach (Transition<string> t in trans)
+                {
+                    routesPresent.Add(t.Symbol);
+                }
+
+                foreach (char letter in dfa.Symbols)
+                {
+                    if (!routesPresent.Contains(letter))
+                    {
+                        int stateToReturnTo = backTrackForWorkingRoute(chars, letter);
+                        dfa.AddTransition(new Transition<string>(state, letter, ogStates[stateToReturnTo]));
+                    }
+                }
+            }
+        }
+
+        private static int BackTrackForWorkingRoute(char[] route, char toUse)
+        {
+            string completeRoute = new string(route);
+
+            for (int i = route.Length - 1; i >= 0; i--)
+            {
+                string tobeAdded = "";
+                for (int j = i; j < route.Length; j++)
+                {
+                    tobeAdded += route[j];
+                }
+
+                string routeTocheck = (toUse.ToString() + tobeAdded);
+                if (routeTocheck.Contains(completeRoute))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static Automata<string> Not(Automata<string> automaat)
+        {
+            Automata<string> notAutomaat = new Automata<string>(automaat.Symbols);
+            //save way of copying transitions
+            notAutomaat.StartStates = automaat.StartStates;
+            foreach (Transition<string> t in automaat.Transitions)
+            {
+                notAutomaat.AddTransition(t);
+            }
+
+            foreach (string state in notAutomaat.States)
+            {
+                if (!automaat.FinalStates.Contains(state))
+                {
+                    notAutomaat.DefineAsFinalState(state);
+                }
+            }
+            return notAutomaat;
+        }
+
+        public enum GeneratorType
+        {
+            BeginsWith,
+            Contains,
+            EndsWith
+        }
+
+        public struct DfaGenerateValue
+        {
+            public bool IsNot;
+            public string Parameter;
+            public GeneratorType Type;
         }
     }
 }
