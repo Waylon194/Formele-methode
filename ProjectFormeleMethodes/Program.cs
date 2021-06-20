a5fe64ee31dcbdd605c4379ffb6d8fdf4156f369
Vincent <Vincent.wjap.de.rooij@outlook.com>
2021-06-20 17:48:11 +0200

    Console Based Type selector

--- ProjectFormeleMethodes/Program.cs
+++ ProjectFormeleMethodes/Program.cs
@@ -4,7 +4,6 @@ using ProjectFormeleMethodes.ConversionEngines.Minimizer;
 using ProjectFormeleMethodes.Examples;
 using ProjectFormeleMethodes.NDFA;
 using ProjectFormeleMethodes.NDFA.Transitions;
-using ProjectFormeleMethodes.Examples;
 using ProjectFormeleMethodes.RegExpressions;
 using ProjectFormeleMethodes.Regular_Expression;
 using System;
@@ -18,28 +17,193 @@ namespace ProjectFormeleMethodes
 
         public static void Main(string[] args)
         {
+            Tester tester = new Tester();
+            ConsoleBuilderSelecterTest();
+        }
+
+        public static RegExp CreateRegExp(int choice, string regexp = "")
+        {
+            switch (choice)
+            {
+                case 1:
+                    return RegExpBuilder.BuildRegExpSampleOne();
+
+                case 2:
+                    return RegExpBuilder.BuildRegExpSampleTwo();
+
+                case 3:
+                    return RegExpBuilder.BuildCustomRegExp(regexp);
+
+                default:
+                    return RegExpBuilder.BuildRegExpSampleTwo();
+            }
+        }
+
+        public static Automata<string> CreateNDFA(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return NDFABuilder.BuildNDFASampleOne();
+
+                case 2:
+                    return NDFABuilder.BuildNDFASampleTwo();
+
+                case 3:
+                    return NDFABuilder.BuildNDFASampleThree();
+
+                case 4:
+                    return NDFABuilder.BuildNDFASampleFour();
+
+                default:
+                    return NDFABuilder.BuildNDFASampleOne();
+            }
+        }
+
+        public static Automata<string> CreateDFA(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return DFABuilder.BuildDFASampleOne();
+
+                case 2:
+                    return DFABuilder.BuildDFASampleOne();
+
+                case 3:
+                    return DFABuilder.BuildDFASampleOne();
+
+                default:
+                    return DFABuilder.BuildDFASampleOne();
+            }
+        }
+
+        public static void ConsoleExecute()
+        {
 
         }
 
-        public static void Testing()
+        public static dynamic ConsoleBuilderSelecterTest()
         {
-            // Testing if the UnionWith methode can merge both SortedSets
-            SortedSet<string> statesPartOne = new SortedSet<string>();
-            statesPartOne.Add("A");
-            statesPartOne.Add("B");
-            statesPartOne.Add("C");
-            statesPartOne.Add("D");
+            Console.WriteLine("Test Formele Methode");
+            Console.WriteLine("Welke optie?");
+            Console.WriteLine("1: RegExp");
+            Console.WriteLine("2: NDFA");
+            Console.WriteLine("3: DFA");
+            Console.WriteLine("-1: Stoppen\n");
 
-            SortedSet<string> statesPartTwo = new SortedSet<string>();
-            statesPartTwo.Add("A");
-            statesPartTwo.Add("B");
-            statesPartTwo.Add("F");
-            statesPartTwo.Add("E");
+            Console.Write("=>> ");
+            int value = -1;
+            int.TryParse(Console.ReadLine(), out value);
 
-            // the answer is yes
-            statesPartOne.UnionWith(statesPartTwo);
+            switch (value)
+            {
+                case 1: // Reguliere expressies
+                    Console.WriteLine("\nReguliere Expressies: ");
+                    Console.WriteLine("Welke optie?");
+                    Console.WriteLine("1: Sample 1");
+                    Console.WriteLine("2: Sample 2");
+                    Console.WriteLine("3: Eigen maken\n");
 
-            Console.WriteLine();//
+                    Console.Write("=>> ");
+                    value = -1;
+                    int.TryParse(Console.ReadLine(), out value);
+
+                    switch (value)
+                    {
+                        case 1:
+                            return CreateRegExp(value);
+
+
+                        case 2:
+                            return CreateRegExp(value);
+
+
+                        case 3:
+                            Console.WriteLine("Maak eigen expressie");
+                            Console.Write(">");
+                            string input = Console.ReadLine();
+                            return CreateRegExp(value, input);
+
+
+                        default:
+                            return CreateRegExp(-1); // back to default        
+                    }
+
+                case 2: // NDFA expressies
+                    Console.WriteLine("\nNDFA Expressies: ");
+                    Console.WriteLine("Welke optie?");
+                    Console.WriteLine("1: Sample 1");
+                    Console.WriteLine("2: Sample 2");
+                    Console.WriteLine("3: Sample 3");
+                    Console.WriteLine("4: Sample 4\n");
+
+                    Console.Write("=>> ");
+                    value = -1;
+                    int.TryParse(Console.ReadLine(), out value);
+
+                    switch (value)
+                    {
+                        case 1:
+                            return CreateNDFA(value);
+
+
+                        case 2:
+                            return CreateNDFA(value);
+
+
+                        case 3:
+                            return CreateNDFA(value);
+
+
+                        case 4:
+                            return CreateNDFA(value);
+
+
+                        default:
+                            return CreateNDFA(-1); // back to default 
+
+                    }
+
+                case 3: // DFA expressies
+                    Console.WriteLine("\nDFA Expressies: ");
+                    Console.WriteLine("Welke optie?");
+                    Console.WriteLine("1: Sample 1");
+                    Console.WriteLine("2: Sample 2");
+                    Console.WriteLine("3: Sample 3\n");
+
+                    Console.Write("=>> ");
+                    value = -1;
+                    int.TryParse(Console.ReadLine(), out value);
+
+                    switch (value)
+                    {
+                        case 1:
+                            return CreateDFA(value);
+
+
+                        case 2:
+                            return CreateDFA(value);
+
+
+                        case 3:
+                            return CreateDFA(value);
+
+
+                        default:
+                            return CreateDFA(-1); // back to default 
+
+                    }
+
+                case -1:
+                    // stoppen met de loop
+                    break;
+
+                default:
+                    ConsoleBuilderSelecterTest();
+                    break;
+            }
+            return null;
         }    
     }
 }
